a658aa4865d1b83fb745d84f4cb59a2d759cc983
Onesun15 <sunnysharma15@hotmail.com>
2017-10-24 15:42:28 -0400

    hook it up

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -5,26 +5,32 @@ const router = express.Router();
 
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
+const mongoose = require('mongoose');
 
 const {BlogPosts} = require('./models');
 
+mongoose.Promise = global.Promise;
+
+router.get('/', (req, res) => res.send('hello world'));
 // convenience function for generating lorem text for blog
 // posts we initially add below
 function lorem() {
   return 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod ' +
-    'tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, '
-    'quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo ' +
+    'tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, ';
+  'quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo ' +
     'consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse ' +
     'cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non ' +
-    'proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    'proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 }
 
 // seed some posts so initial GET requests will return something
-BlogPosts.create(
-  '10 things -- you won\'t believe #4', lorem(), 'Billy Bob');
-BlogPosts.create(
-  'Lions and tigers and bears oh my', lorem(), 'Lefty Lil');
+// BlogPosts.create(
+//   '10 things -- you won\'t believe #4', lorem(), 'Billy Bob');
+// BlogPosts.create(
+//   'Lions and tigers and bears oh my', lorem(), 'Lefty Lil');
 
+
+  
 // add endpoint for GET. It should call `BlogPosts.get()`
 // and return JSON objects of stored blog posts.
 // send back JSON representation of all blog posts
@@ -49,3 +55,5 @@ BlogPosts.create(
 // add endpoint for DELETE requests. These requests should
 // have an id as a URL path variable and call
 // `BlogPosts.delete()`
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+'use strict';
+
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
-
+const mongooseRouter = require('./blogPostsRouter');
+//const { PORT } = require('./config');
 
 app.use(morgan('common'));
+app.use('/blog-posts', mongooseRouter);
 
-// you need to import `blogPostsRouter` router and route
-// requests to HTTP requests to `/blog-posts` to `blogPostsRouter`
+app.use(bodyParser.json());
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
